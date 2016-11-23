31cf7186913b699f4a8e72104cc18df76a04efa9
Yukio Aoyama <aoyyuk@gmail.com>
2016-11-23 21:28:54 +0900

    Initial Commit

---
+++ Poke Memo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poke Memo
+//
+//  Created by 青山 行夫 on 2016/11/23.
+//  Copyright © 2016年 mm289. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
